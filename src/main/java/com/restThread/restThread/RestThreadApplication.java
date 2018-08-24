264e383be9017372a02ef873d543ada1677e7418
Your Name <you@example.com>
2018-08-24 11:49:51 -0300

    first commit

---
+++ src/main/java/com/restThread/restThread/RestThreadApplication.java
@@ -0,0 +1,12 @@
+package com.restThread.restThread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestThreadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestThreadApplication.class, args);
+	}
+}
